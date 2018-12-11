1df700b2f0c5ca680938c9267988c069d8a176cd
nidkil <info@nidkil.com>
2018-12-11 01:22:19 +0100

    Release 0.1.0

--- dist/use-pkg-version.es.js
+++ dist/use-pkg-version.es.js
@@ -1,10 +1,11 @@
 /*!
- * use-pkg-version v0.0.0
+ * use-pkg-version v0.1.0
  * (c) 2018-present nidkil <info@nidkil.com> (https://www.nidkil.com)
  * Released under the MIT License.
  */
 import fs from 'fs';
 import path from 'path';
+import cosmiconfig from 'cosmiconfig';
 import slash from 'slash';
 import commander from 'commander';
 import chalk from 'chalk';
@@ -132,8 +133,36 @@ var fsHelpers = {
   rmdirRecursive
 };
 
+/**
+ * Starting from the directory specified by startDir it will try to load a configuration file that contains a
+ * configuration object in the following places:
+ * 1. A 'moduleName' property in the package.json file
+ * 2. A '.<moduleName>rc' file with JSON or YAML syntax
+ * 3. A '.<moduleName>rc.json' file
+ * 4. A '.<moduleName>rc.yaml', '.<moduleName>rc.yml' or '.<moduleName>rc.js' file
+ * 5. A '<moduleName>.config.js' file
+ *
+ * @param {string} startDir - The directory to look for the configuration file
+ * @param {string} moduleName - The name of the module the configuration belongs to
+ * @param {Object} defaults - Default configuration values
+ * @returns {Object} The configuration object
+ */
+
+
+function loadConfig(startDir, moduleName, defaults = {}) {
+  const explorer = cosmiconfig(moduleName);
+
+  const _ref = explorer.searchSync(startDir) || {},
+        _ref$config = _ref.config,
+        config = _ref$config === void 0 ? {} : _ref$config;
+
+  return Object.assign({}, defaults, config);
+}
+
+var loadConfig_1 = loadConfig;
+
 var name = "use-pkg-version";
-var version = "0.0.0";
+var version = "0.1.0";
 var description = "Update any static file with the version number from the package.json file";
 var keywords = [
 	"javascript",
@@ -152,10 +181,11 @@ var bugs = {
 };
 var homepage = "https://github.com/nidkil/use-pkg-version#readme";
 var bin = "./bin/use-pkg-version.js";
-var main = "src/use-pkg-version-cli.js";
-var entry = "./src/use-pkg-version-cli.js";
-var browser = "./dist/use-pkg-version.js";
-var unpkg = "./dist/use-pkg-version-min.js";
+var entry = "./src/use-pkg-version.cli.js";
+var main = "./src/use-pkg-version.cli.cjs.js";
+var module$1 = "./src/use-pkg-version.cli.es.js";
+var browser = "./dist/use-pkg-version.min.js";
+var unpkg = "./dist/use-pkg-version.min.js";
 var files = [
 	"LICENSE.md",
 	"README.md",
@@ -166,9 +196,12 @@ var files = [
 	"*.json"
 ];
 var scripts = {
-	test: "jest",
+	test: "DEBUG=stdout-stderr & jest",
+	coverage: "npm run test -- --coverage",
+	coveralls: "npm run coverage && cat ./coverage/lcov.info | coveralls",
 	lint: "eslint -c .eslintrc.js --format codeframe bin src tests",
 	"lint:fix": "eslint --fix -c .eslintrc.js --format codeframe bin src tests",
+	"lint:error-only": "eslint -c .eslintrc.js --quiet --format codeframe src tests bin",
 	"lint:check": "eslint --print-config .eslintrc.js | eslint-config-prettier-check",
 	"cz:commit": "git cz",
 	"cz:retry": "git cz --retry",
@@ -176,7 +209,8 @@ var scripts = {
 	"commitlint:last": "commitlint --edit",
 	"git:first": "git rev-list HEAD | tail -n 1",
 	"git:last": "git rev-list HEAD | head -n 1",
-	build: "bili --config .bili.config.json"
+	build: "rm -rf dist && bili --config .bili.config.json",
+	release: "nodenv --env ./.env.local --exec release-it --verbose"
 };
 var engines = {
 	node: ">=6"
@@ -184,6 +218,7 @@ var engines = {
 var dependencies = {
 	chalk: "^2.4.1",
 	commander: "^2.19.0",
+	cosmiconfig: "^5.0.7",
 	envinfo: "^6.0.1",
 	"replace-in-file": "^3.4.2",
 	slash: "^2.0.0",
@@ -226,8 +261,9 @@ var _package = {
 	bugs: bugs,
 	homepage: homepage,
 	bin: bin,
-	main: main,
 	entry: entry,
+	main: main,
+	module: module$1,
 	browser: browser,
 	unpkg: unpkg,
 	files: files,
@@ -249,8 +285,9 @@ var _package$1 = /*#__PURE__*/Object.freeze({
 	bugs: bugs,
 	homepage: homepage,
 	bin: bin,
-	main: main,
 	entry: entry,
+	main: main,
+	module: module$1,
 	browser: browser,
 	unpkg: unpkg,
 	files: files,
@@ -264,10 +301,14 @@ var _package$1 = /*#__PURE__*/Object.freeze({
 
 var require$$0 = getCjsExportFromNamespace(_package$1);
 
-const directoryExists$1 = fsHelpers.directoryExists,
-      fileExists$1 = fsHelpers.fileExists;
+const fileExists$1 = fsHelpers.fileExists;
+
+
+
+let verbose = false;
+let debug = true;
 const defaults = {
-  name: 'use-pkg-version',
+  name: require$$0.name,
   packageFile: 'package.json',
   commands: ['update', 'info']
 };
@@ -282,9 +323,10 @@ function camelcase(str) {
 // camelcase them and remove leading dashes
 
 
-function cleanArgs(cmd) {
+function cleanArgs(cmd, file = null) {
   const args = {
-    cmd: cmd._name
+    cmd: cmd._name,
+    file
   };
   cmd.options.forEach(opt => {
     const key = camelcase(opt.long.replace(/^--/, '')); // If an option is not present and Command has a method with the same name it should not be copied
@@ -299,57 +341,55 @@ function cleanArgs(cmd) {
 
 
 function enrichArgs(args) {
-  const dest = args && args.dest ? args.dest : defaults.buildDestDir;
-  const buildDestPath = slash(path.join(process.cwd(), dest));
-  args.buildDestPath = buildDestPath;
+  const packageFile = args && args.packageFile ? args.packageFile : defaults.packageFile;
+  args.packageFile = slash(path.join(process.cwd(), packageFile));
 
   if (args && args.file) {
-    const filePath = slash(path.join(args.buildDestPath, args.file));
-    args.filePath = filePath;
+    const updateFile = slash(path.join(process.cwd(), args.file));
+    args.updateFile = updateFile;
   }
 
+  debug && console.log('enrichArgs', JSON.stringify(args, null, '\t'));
   return args;
 } // Lets check that directories and files exist, so that individual commands do not have to handle this themselves
 
 
 function checkArgs(args) {
   if (args) {
-    const verbose = args.verbose || false;
-
-    if (args.buildDestPath && !directoryExists$1(args.buildDestPath)) {
-      verbose && console.log(args.cmd, JSON.stringify(args, null, '\t'));
-      console.log(chalk.red('Build destination directory does not exist: ' + args.buildDestPath));
-      process.exit(-1);
+    if (args.packageFile && !fileExists$1(args.packageFile)) {
+      debug && console.log('checkArgs', JSON.stringify(args, null, '\t'));
+      console.error(chalk.red('package.json not found: ' + args.packageFile));
+      process.exit(1);
     }
 
     if (args.filePath && !fileExists$1(args.filePath)) {
-      verbose && console.log(args.cmd, JSON.stringify(args, null, '\t'));
-      console.log(chalk.red('File does not exist: ' + args.filePath));
-      process.exit(-1);
+      debug && console.log('checkArgs', JSON.stringify(args, null, '\t'));
+      console.error(chalk.red('File does not exist: ' + args.filePath));
+      process.exit(2);
     }
   }
 
   return args;
-}
+} // Merge the args and config, args overrule config options
 
-function isHelpFlagSet() {
-  return process.argv.includes('--help') || process.argv.includes('-h');
+
+function mergeArgsAndConfig(args, config) {
+  const merged = Object.assign({}, config, args);
+  debug && console.log('mergeArgsAndConfig\nargs:', JSON.stringify(args, null, '\t'), ',\nconfig:', JSON.stringify(config, null, '\t'), ',\nmerged:', JSON.stringify(merged, null, '\t'));
+  return merged;
 }
 
 function cli() {
   commander.version(require$$0.version).usage('<command> [options]');
-  commander.command('update <file>').description('update the version number in the specified file with the version number from the package.json file, the file must be relevant to the current directory').option('-p, --packageFile', `path to the package.json file relevant to the current directory, default '${defaults.packageFile}'`, defaults.packageFile).option('-d, --dry', 'dry run, only shows which file will be updated but does not actually update anything').option('-v, --verbose', 'show processing information, default false').option('-q, --quiet', 'report errors only, default false').option('-D, --debug', 'show debugging information, default false').action(function (file, options) {
-    // Workaround so process.exit can be suppressed when unit testing
-    {
-      console.log('file: ' + JSON.stringify(file));
-      console.log('options: ' + JSON.stringify(cleanArgs(options)));
-      console.log('argv: ' + JSON.stringify(process.argv));
-      console.log('isHelpFlagSet: ' + isHelpFlagSet());
-    }
+  commander.command('update <file>').description('update the version number in the specified file with the version number from the package.json file, the file must be relevant to the current directory').option('-p, --package-file [filename]', 'path to the package.json file relevant to the current directory, default ' + `'${defaults.packageFile}'`, defaults.packageFile).option('-d, --dry', 'dry run, only shows which file will be updated but does not actually update anything').option('-c, --config', 'configuration file that contains the string to search for and replace with').option('-s, --search-for [regex]', 'string to search for, can be a regex expression').option('-r, --replace-with [string]', 'value to replace with, must contain the {{version}} placeholder for the version number').option('-v, --verbose', 'show processing information, default false').option('-q, --quiet', 'report errors only, default false').option('-D, --debug', 'show debugging information, default false').action(function (file, options) {
+    const config = loadConfig_1(process.cwd(), defaults.name);
+    debug = options.debug || config.debug || false;
+    verbose = options.verbose || config.verbose || false;
+    debug && console.log('loadConfig', JSON.stringify(config, null, '\t'));
 
-    if (!isHelpFlagSet()) {
-      commonjsRequire(createCmdModule(options))(checkArgs(enrichArgs(cleanArgs(options))));
-    }
+    const cmd = commonjsRequire(createCmdModule(options));
+
+    cmd(checkArgs(enrichArgs(mergeArgsAndConfig(cleanArgs(options, file), config))));
   });
   commander.command('info').description('print debugging information about your environment').action(cmd => {
     console.log(chalk.bold('\nEnvironment Info:'));

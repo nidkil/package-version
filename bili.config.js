c3a3741a99a5c248e859763c52331c804696ec36
nidkil <info@nidkil.com>
2018-12-16 14:59:27 +0100

    build: removed umd
    No support for umd as library should not be used in the browser.

--- bili.config.js
+++ bili.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   input: './src/use-pkg-version-cli.js',
   outDir: './dist',
-  format: ['es', 'umd', 'umd-min', 'cjs'],
-  moduleName: 'use-pkg-version',
+  format: ['es', 'cjs'],
   name: 'use-pkg-version',
   filename: '[name][suffix].js',
   banner: true,
   target: 'node',
-  alias : {
+  alias: {
     '@': require('path').resolve(__dirname, 'src')
   }
 }
